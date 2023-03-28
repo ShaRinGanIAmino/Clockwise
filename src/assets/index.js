8317baa61c7f680e5638167a54615715fda77977
Amine Chabbeh <aminoschabbeh12345@gmail.com>
2023-03-28 17:41:03 +0100

    first commit

---
+++ src/assets/index.js
@@ -0,0 +1,33 @@
+import men1 from './men1.png';
+import men2 from './men2.png';
+import men3 from './men3.png';
+import men4 from './men4.png';
+import new1 from './new1.png';
+import new2 from './new2.png';
+import new3 from './new3.png';
+import new4 from './new4.png';
+import women1 from './women1.png';
+import women2 from './women2.png';
+import women3 from './women3.png';
+import women4 from './women4.png';
+import hero from './hero.png';
+import logo from './logo.svg';
+
+
+export {
+    men1,
+    men2,
+    men3,
+    men4,
+    women1,
+    women2,
+    women3,
+    women4,
+    new1,
+    new2,
+    new3,
+    new4,
+    logo,
+    hero,
+};
+
---
+++ tailwind.config.cjs
@@ -0,0 +1,26 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./index.html', './src/**/*.{js,jsx}'],
+  mode: 'jit',
+  theme: {
+    extend: {
+      colors: {
+        primary: '#0E0E0E',
+        secondary: '#00f6ff',
+      },
+      fontFamily: {
+        poppins: ['Poppins', 'sans-serif'],
+        mont: ['Montserrat', 'sans-serif'],
+      },
+    },
+    screens: {
+      xs: '480px',
+      ss: '620px',
+      sm: '768px',
+      md: '1060px',
+      lg: '1200px',
+      xl: '1700px',
+    },
+  },
+  plugins: [],
+};
