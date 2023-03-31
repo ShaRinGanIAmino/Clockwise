2b7489f0d90547b2541ac5ba2556c265b532d811
Chebbah Mohamed Amine <amino@Chebbahs-MacBook-Pro.local>
2023-03-31 02:26:35 +0100

    test

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,5 +3,6 @@ import Home from "./Home.jsx";
 import Men from "./Men.jsx";
 import Women from "./Women.jsx";
 import Basket from "./Basket.jsx";
+import Product from "./Product.jsx";
 
-export { Home, Women, Men, Contact, Basket };
+export { Home, Women, Men, Contact, Basket, Product };
